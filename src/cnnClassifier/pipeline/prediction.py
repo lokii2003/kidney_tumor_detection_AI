c60ac77d5fbfc1bc173cd80fb3d05c94ede3df8b
lokii2003 <lk3389815@gmail.com>
2026-04-04 14:45:55 +0530

    Aap Created

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow
 from keras.models import load_model
 from keras.preprocessing import image
+
 import os
 
 
@@ -23,7 +24,7 @@ class PredictionPipeline:
 
 
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.load_img(imagename, target_size=(224,224))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = np.argmax(model.predict(test_image), axis=1)
